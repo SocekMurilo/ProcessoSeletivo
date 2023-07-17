0e84b4500b051cdb744371eaada503a89d677823
Juan Campos <juancampos.chk@gmail.com>
2023-07-17 09:35:40 -0300

    Funcionando pro Murilo

---
+++ Juan/public/js/filtroParticipou.js
@@ -0,0 +1,48 @@
+const { ParticipanteProcesso } = require('../src/model/participanteProcesso');
+
+// Manipular o clique no botão "Aplicar filtros"
+$("#aplicarFiltro").click(async function() {
+  // Verificar qual opção de filtro está selecionada
+  var radioValue = $("input[name='vbtn-radioParticipou']:checked").attr("id");
+
+  // Filtrar os participantes com base na opção selecionada
+  if (radioValue === "vbtn-radioParticipou1") {
+    // Participantes que já participaram de outro processo
+    const participantes = await ParticipanteProcesso.findAll({
+      group: ['IDParticipante'],
+      having: sequelize.literal('COUNT(*) > 1')
+    });
+
+    // Obter os IDs dos participantes que participaram de outros processos
+    const participantesIds = participantes.map(participante => participante.IDParticipante);
+
+    // Exibir apenas os participantes que estão na lista de IDs filtrados
+    $(".participante-item").each(function() {
+      var idParticipante = $(this).data("idparticipante");
+      if (participantesIds.includes(idParticipante)) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  } else if (radioValue === "vbtn-radioParticipou2") {
+    // Participantes que não participaram de outro processo
+    const participantes = await ParticipanteProcesso.findAll({
+      group: ['IDParticipante'],
+      having: sequelize.literal('COUNT(*) = 1')
+    });
+
+    // Obter os IDs dos participantes que não participaram de outros processos
+    const participantesIds = participantes.map(participante => participante.IDParticipante);
+
+    // Exibir apenas os participantes que estão na lista de IDs filtrados
+    $(".participante-item").each(function() {
+      var idParticipante = $(this).data("idparticipante");
+      if (participantesIds.includes(idParticipante)) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  }
+});
--- Juan/routes.js
+++ Juan/routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 // Importando os Controllers
 const novoProcesso = require('./src/controllers/novoProcesso');
-const home = require('./src/controllers/home');
+const editarProcesso = require('./src/controllers/editarProcesso');
 
-router.post('/novoProcesso', novoProcesso);
+const home = require('./src/controllers/home');
 
 // Iniciando as rotas
 router.get('/', home.pagInicialGet);
@@ -15,5 +15,8 @@ router.get('/participantes', home.participantesGet);
 router.get('/participante/:IDParticipante', home.participanteGet);
 router.get('/grupos', home.gruposGet);
 router.get('/:IDProcesso', home.processoGet);
+
+router.post('/novoProcesso', novoProcesso);
+router.post('/editarProcesso/:IDProcesso', editarProcesso);
 
 module.exports = router;
